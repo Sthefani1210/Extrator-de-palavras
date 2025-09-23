afc8c16053c4b54428a0b15a1dbd7349f6b01382
Sthefani1210 <paula.sthefani@escola.pr.gov.br>
2025-09-23 09:40:36 -0300

    terminado aula 5

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-
 const botaoMostraPalavras = document.querySelector( "#botao-paçavrachave");
 
 botaoMostraPalavras.addEventListener( " click", mostraPalavrasChave);
 
 function mostraPalavrasChave(){ 
     const texto = document.querySelector( "#entrada-de-texto").ariaValueMax;
-    
     const campoResultado = document. querySelector( "#resultado-palavrachave");
-
     const mostraPalavrasChave = processaTexto(texto);
 
     campoResultado.textoContent = palavras.join(","); 
@@ -15,5 +12,15 @@ function mostraPalavrasChave(){
 
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
+
+    let frequencias = {};
+    for (let i of palavras) {
+        frequencias [i] =0; 
+    for(let j of palavras){
+        if(i==j) {
+            frequencias[i]++;
+        }
+        }
+    }
     return palavras;
 }
