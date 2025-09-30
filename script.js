f9c16df361a20881113fb166edc5696b5417f05c
Sthefani1210 <paula.sthefani@escola.pr.gov.br>
2025-09-30 09:46:08 -0300

    aula 6 terminado

--- script.js
+++ script.js
@@ -12,7 +12,17 @@ function mostraPalavrasChave(){
 
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavras);
 
+    function ordenaPalavras(p1, p2){
+        return frequencias[p2] - frequencias[p1];
+    }
+
+    console.log(ordenadas);
+    return ordenadas.slice(0,10);
+}
+function contaFrequencias(palavras){
     let frequencias = {};
     for (let i of palavras) {
         frequencias [i] =0; 
